7ea65ca915e1accc8452a9c20fe7bf22780fc6b7
Asma <as1588ma@gmail.com>
2022-03-27 21:13:31 -0400

    commit tree 4747af364d11d3dda9d3681e6dc206d85cd320e6

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // code your solution here
+function saturdayFun(param1) {
+  if (param1 === "bathe my dog") {
+    return "This Saturday, I want to bathe my dog!";
+  }
+  return "This Saturday, I want to roller-skate!";
+}
+
+function mondayWork(param2) {
+  if (param2 === "work from home") {
+    return "This Monday, I will work from home.";
+  }
+
+  return "This Monday, I will go to the office.";
+}
+function wrapAdjective(param1) {
+  if (param1 === "*") {
+    return function (innerParam) {
+      return `You are *${innerParam}*!`;
+    };
+  }
+  if (param1 === "||") {
+    return function (innerParam) {
+      return `You are ||${innerParam}||!`;
+    };
+  }
+}
